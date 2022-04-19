15a646c9343145192e13820b40f125b7fd7dbab7
jainakshat425 <42264584+jainakshat425@users.noreply.github.com>
2022-04-19 07:12:25 +0530

    Create Solution.java

---
+++ Recursion I/Search in a Binary Search Tree/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        if( root == null || root.val == val ) return root;
+        
+        return searchBST( val < root.val ? root.left : root.right, val);
+    }
+}
