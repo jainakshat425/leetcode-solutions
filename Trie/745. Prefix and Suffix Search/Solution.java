0676078e4c54dd03fa8c686dc0344b960ddc7c6a
Akshat Jain <42264584+jainakshat425@users.noreply.github.com>
2022-06-18 12:25:37 +0530

    Update Solution.java

--- Trie/745. Prefix and Suffix Search/Solution.java
+++ Trie/745. Prefix and Suffix Search/Solution.java
@@ -7,6 +7,7 @@ class WordFilter {
         trie = new Trie();
         
         for(int i=0; i<n; i++) {
+			// for word = abc, store in trie -> c#abc, bc#abc, abc#abc
             int len = words[i].length();
             StringBuilder curr = new StringBuilder(words[i]);
             curr.insert(0, '#');
@@ -25,12 +26,14 @@ class WordFilter {
     }
 }
 class TrieNode {
+	// 26 for lowercase letters and 1 for # (stored at last index)
     private static final int N = 27;
     private TrieNode[] links;
     private int wordIndex;
     
     public TrieNode() {
         links = new TrieNode[N];
+		// at any time, it will store the index of the last word which has the current prefix
         wordIndex = -1;
     }
     
@@ -95,98 +98,3 @@ class Trie {
         return curr.getWordIndex();
     }
 }
-// class WordFilter {
-
-//     private int n;
-//     private String[] words;
-//     private Trie trie;
-    
-//     public WordFilter(String[] words) {
-//         this.n = words.length;
-//         this.words = words;   
-//         this.trie = new Trie();
-        
-//         for(int i=n-1; i>=0; i--) 
-//             trie.insert(words[i], i);
-//     }
-    
-//     public int f(String prefix, String suffix) {
-//         return trie.search(prefix, suffix, words);
-//     }
-// }
-// class TrieNode {
-//     private static final int N = 26;
-//     private TrieNode[] links;
-//     private List<Integer> indices;
-    
-//     public TrieNode() {
-//         links = new TrieNode[N];
-//         indices = new ArrayList();
-//     }
-    
-//     public boolean containsKey(char c) {
-//         return links[c - 'a'] != null;
-//     }
-    
-//     public TrieNode get(char c) {
-//         return links[c - 'a'];
-//     }
-    
-//     public void put(char c, TrieNode node) {
-//         links[c - 'a'] = node;
-//     }
-    
-//     public void addIndex(int ind) {
-//         indices.add(ind);
-//     }
-    
-//     public List<Integer> getIndices() {
-//         return indices;
-//     }
-// }
-// class Trie {
-//     private TrieNode root;
-    
-//     public Trie() {
-//         root = new TrieNode();
-//     }
-    
-//     public void insert(String word, int ind) {
-//         int n = word.length();
-//         TrieNode curr = root;
-        
-//         for(int i=0; i<n; i++) {
-//             char c = word.charAt(i);
-            
-//             if( !curr.containsKey(c) ) 
-//                 curr.put(c, new TrieNode());
-            
-//             curr = curr.get(c);
-//             curr.addIndex(ind);
-//         }
-//     }
-    
-//     public int search(String prefix, String suffix, String[] words) {
-//         int n = prefix.length();
-//         TrieNode curr = root;
-        
-//         for(int i=0; i<n; i++) {
-//             char c = prefix.charAt(i);
-            
-//             if( !curr.containsKey(c) ) 
-//                 return -1;
-            
-//             curr = curr.get(c);
-//         }            
-//         for(int ind : curr.getIndices()) {
-//             if( words[ind].endsWith(suffix) )
-//                 return ind;
-//         }
-//         return -1;
-//     }
-// }
-/**
- * Your WordFilter object will be instantiated and called as such:
- * WordFilter obj = new WordFilter(words);
- * int param_1 = obj.f(prefix,suffix);
- */
