ebc0892c722d7e1f66665753aa8ecbe85df71ddd
Akshat Jain <42264584+jainakshat425@users.noreply.github.com>
2022-07-06 09:28:24 +0530

    Create Solution.java

---
+++ Binary Search Tree/938. Range Sum of BST/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if( root == null )
+            return 0;
+        
+        if( root.val >= low && root.val <= high ) 
+            return root.val + 
+                rangeSumBST(root.left, low, high) + 
+                rangeSumBST(root.right, low, high);
+        
+        else if( root.val < low )
+            return rangeSumBST(root.right, low, high);
+        
+        else
+            return rangeSumBST(root.left, low, high);
+    }
+}
